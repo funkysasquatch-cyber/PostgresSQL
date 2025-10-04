06dbe22a46ed64d0536f6f29e8b5e7d23ad9a786
Funky Sasquatch <funkysasquatchxyz@gmail.com>
2025-10-04 10:50:19 +0700

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -57,3 +57,48 @@ where id in ('P001', 'P005', 'P010');
 select * from product
 where date(created_at) = current_date;
 
+--update pake whereclouse
+update product
+set price = 9000000
+where id = 'P001';
+
+update product
+set quantity = 5
+where quantity = 0;
+
+update product
+set price = price * 1.1
+where price > 1000000;
+
+update product
+set name = 'Laptop Lenovo ThinkPad'
+where id = 'P001';
+
+-- untuk data table
+delete from product
+where id = 'P010';
+
+-- alias
+select 
+    id as kode_produk,
+    name as nama_produk,
+    price as harga,
+    quantity as stok
+from product;
+
+
+select p.id, p.name, p.price
+from product p;
+
+select 
+    name,
+    price,
+    quantity,
+    price * quantity as total_nilai
+from product;
+
+select p.name as produk, s.name as supplier
+from product p
+join supplier s on p.id = s.product_id;
+
+
